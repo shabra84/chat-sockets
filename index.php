2ece7e995b9d781b2507953d725b2e67461923c0
Fernando Mangas <fernandomangasmolteni@gmail.com>
2020-03-14 09:17:46 +0100

    subo archivos

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>sendme!</title>
+    </head>
+<body>
+    
+    <div id="chat"></div>
+    <input id="name" type="text" placeholder="name" />
+    <input id="message" type="text" placeholder="message" />
+    <button id="send">Send</button>
+
+    <script src="librerias/cliente.js"></script>
+    
+</body>
+
+</html>
